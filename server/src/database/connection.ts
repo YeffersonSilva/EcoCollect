2acf35603f83bdfe39b09d3a023979bed53dfd57
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-08-01 15:48:00 -0300

    feat: Create migrations for points, items, and point_item tables
    - Added migrations for the points table.
    - Added migrations for the items table.
    - Added migrations for the point_item table.

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,12 +1,12 @@
-import knex from "knex";
+import { Knex } from "knex";
 import path from "path";
 
-const connection = knex({
+const config: Knex.Config = {
     client: 'sqlite3',
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite')
     },
+    useNullAsDefault: true  // SQLite requires this option to handle null values correctly
+};
 
-})
-
-export default connection;
+export default config;
